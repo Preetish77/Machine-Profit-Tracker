4efe37fc3310de942ab26a4a3533b130e35c31ba
Preetish77 <106698309+Preetish77@users.noreply.github.com>
2026-03-05 11:18:02 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME="machine-tracker";
+
+const urls=[
+"/",
+"/index.html"
+];
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache=>cache.addAll(urls))
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request)
+.then(res=>res||fetch(e.request))
+);
+});
